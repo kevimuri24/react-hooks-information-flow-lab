05581b7af665678be6e509c2a79d38cf6b65f4a9
Kevin Muriuki Karimi <kevimuri24@gmail.com>
2024-09-07 17:14:28 +0300

    Finished Lab

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header({ onDarkModeClick }) {
+   return <div>
+        <header>
+            <h2>Shopster</h2>
+            <button onClick={ onDarkModeClick}>
+            {onDarkModeClick ? "Dark" : "Light"} Mode
+            </button>
+        </header>
+    </div>
+    
+}
+
+export default Header;
